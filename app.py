aca6fed13ec3c6539cf60fc7e75528d2acce6198
kss239 <36244677+kss239@users.noreply.github.com>
2022-12-03 15:49:35 -0600

    Update app.py streamlit title

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from sklearn.preprocessing import StandardScaler
 
 pima = pd.read_csv("diabetes.csv")
 
-st.title('Determining Diabetes in Pima Native Americans')
+st.title('Diabetes in Pima Native Americans')
 st.header('Context')
 st.write('Diabetes is one of the most frequent diseases worldwide and the number of diabetic patients are growing over the years. The main cause of diabetes remains unknown, yet scientists believe that both genetic factors and environmental lifestyle play a major role in diabetes. A few years ago research was done on a tribe in America which is called the Pima tribe. In this tribe, it was found that the women are prone to diabetes very early. Several constraints were placed on the selection of these instances from a larger database. In particular, all patients were females at least 21 years old of Pima Indian heritage.')
 
